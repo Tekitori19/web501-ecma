9b73bd7fb14440e6c07703e89d12a80b6025da42
Dwcks <dinhpdpd09993@fpt.edu.vn>
2024-11-26 18:48:24 +0700

    added serve static for BE

--- frontends/js/services/ProductService.js
+++ frontends/js/services/ProductService.js
@@ -1,22 +1,22 @@
 export class ProductService {
     async getProducts(filters = {}) {
-        let url = 'http://localhost:3000/products?';
-        
+        let url = 'http://localhost:8080/products?';
+
         if (filters.cate_id) {
             url += `cate_id=${filters.cate_id}&`;
         }
-        
+
         const response = await fetch(url);
         let products = await response.json();
-        
+
         if (filters.minPrice) {
             products = products.filter(p => p.price >= filters.minPrice);
         }
-        
+
         if (filters.maxPrice) {
             products = products.filter(p => p.price <= filters.maxPrice);
         }
-        
+
         return products;
     }
 
--- src/controllers/view.rs
+++ src/controllers/view.rs
@@ -1,9 +1,20 @@
 use axum::{response::Html, routing::get, Router};
+use std::fs;
+use std::path::Path;
+use tower_http::services::ServeDir;
 
 pub fn view_service() -> Router {
-    Router::new().route("/", get(handler_html))
+    Router::new()
+        .route("/", get(handler_html))
+        .nest_service("/static", ServeDir::new("frontends"))
+}
+
+fn import_html(page_name: &str) -> String {
+    let format_path = &format!("frontends/{}.html", page_name);
+    let path = Path::new(format_path.as_str());
+    fs::read_to_string(path).expect("Unable to read file")
 }
 
 pub async fn handler_html() -> Html<String> {
-    Html("<h2>views page</h2>".to_string())
+    Html(import_html("index"))
 }
